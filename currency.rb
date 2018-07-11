c6afa6f7eb607e76a4d5cb5dfcf686838060b2c8
katesliang <kateliangg@gmail.com>
2018-07-11 14:16:19 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,21 +1,5 @@
 # Write your code here
 
-
-puts "How much money do you want to convert?"
-amount = gets.chomp().to_i
- 
-puts "Pick from the following menu:"
-puts "1. USD to EUR"
-puts "2. USD to JPY"
-puts "3. USD to GBP"
-puts "4. USD to AUD"
-puts "5. EUR to USD"
-puts "6. JPY to USD"
-puts "7. GBP to USD"
-puts "8. AUD to USD"
- 
-choice = gets.chomp.to_i
-
 def usd_to_eur(amount)
   new_currency = amount * 0.85
 end
@@ -47,6 +31,23 @@ end
 def aud_to_usd(amount)
   new_currency = dollars / 1.34
 end
+
+
+ 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+ 
+choice = gets.chomp.to_i
+
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i
 
 case choice
 when 1
